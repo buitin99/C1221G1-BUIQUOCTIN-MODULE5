119c99d3f1267cfc0f06337e1a89f84fcdc108ef
buitin99 <buiquoctin.job@gmail.com>
2022-06-08 17:16:09 +0700

    update task 2 casestudy

--- case-study-angular/src/app/app.module.ts
+++ case-study-angular/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FacilityComponent } from './facility/facility.component';
+import { ContractComponent } from './contract/contract.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    FacilityComponent,
+    ContractComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
